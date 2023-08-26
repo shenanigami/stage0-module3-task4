1a9934f74fa81e4d2dabc29687e2ba979bacc4be
shenanigami <38479116+shenanigami@users.noreply.github.com>
2023-08-26 15:31:35 +0600

    First batch of changes

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r = 114;
+
+        System.out.println(c + "" + h + "" + a + "" + r);
     }
 }
